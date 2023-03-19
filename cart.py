b7626551b68583f6db6d8e4cbc6754f0fdbd814d
Julia <juliatelesinska303@gmail.com>
2023-03-19 18:23:34 +0100

    classes and main changes

--- cart.py
+++ cart.py
@@ -1,2 +1,63 @@
-#TODO
-# implement functionality
+class Product:
+    all_products = []
+
+    def __init__(self, name: str, price: int, amount: int):
+        self.name = name
+        self.price = price
+        self.amount = amount
+        Product.all_products.append(self)
+    
+    def __str__(self):
+        return f"Product name: {self.name},\
+            \nProduct price: ${self.price},\
+            \nAmount available: {self.amount}."
+    
+    def __repr__(self):
+        return self.name
+
+class ShoppingCart:
+    def __init__(self, budget: int):
+        self.budget = budget
+        self.items_in_cart = {}
+    
+    #TODO take amount declared inside Product class into account
+    # when adding product to cart
+    def add_to_cart(self, item: Product, amount: int):
+        if item.name not in self.items_in_cart:
+            self.items_in_cart[item.name] = (amount, amount*item.price)
+        else:
+            current_amount, current_total_price = self.items_in_cart[item.name]
+            new_amount = current_amount + amount
+            new_total_price = current_total_price + item.price * amount
+            self.items_in_cart[item.name] = (new_amount, new_total_price)
+    
+    def show_cart(self):
+        for item, amount_price in self.items_in_cart.items():
+            print(f"> {amount_price[0]} * {item} = ${amount_price[1]}")
+
+    def calculate_budget(self):
+        total_price = 0
+        for item, amount_price in self.items_in_cart.items():
+            total_price += amount_price[1]
+        if total_price > self.budget:
+            print(f"You're ${total_price-self.budget} over budget! Your total is ${total_price}.")
+        else:
+            print(f"Your total is ${total_price}.")
+
+
+milk = Product("milk", 5, 20)
+eggs = Product("eggs", 10, 12)
+shrimp = Product("shrimp", 20, 5)
+spinach = Product("spinach", 3, 10)
+cat_food = Product("cat food", 2, 8)
+bread = Product("bread", 3, 2)
+beef = Product("beef", 9, 6)
+
+if __name__ == "__main__":
+    print(Product.all_products)
+    cart = ShoppingCart(80)
+    cart.add_to_cart(milk, 15)
+    cart.add_to_cart(milk, 1)
+    cart.add_to_cart(eggs, 2)
+    cart.show_cart()
+    cart.calculate_budget()
--- main.py
+++ main.py
@@ -1,1 +1,40 @@
 import speech_recognition as sr
+import pyttsx3
+from cart import Product, ShoppingCart
+
+# setting up the voice assistant
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+r = sr.Recognizer()
+
+
+def set_budget():
+    with sr.Microphone() as source:
+        print("Please state your budget:")
+        engine.say("Please state your budget.")
+        engine.runAndWait()
+        audio = r.listen(source)
+
+    # Use speech recognition to convert the user's response to text
+    try:
+        budget = int(r.recognize_google(audio))
+        print(f"Your budget is {budget}")
+        engine.say(f"Your budget is {budget}.")
+        engine.runAndWait()
+        my_cart = ShoppingCart(budget)
+        print(my_cart.budget)
+        return budget
+    
+    except sr.UnknownValueError:
+        print("Sorry, I didn't understand that. Please try again.")
+        engine.say("Sorry, I didn't understand that. Please try again.")
+        engine.runAndWait()
+        return set_budget()
+    
+def go_shopping():
+    pass
+
+if __name__ == "__main__":
+    set_budget()
